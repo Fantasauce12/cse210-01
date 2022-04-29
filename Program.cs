e429a0a1bb032e3bc61da8e5d52586cf190a5a6f
Jax McKinney <mck20018@byui.edu>
2022-04-28 20:10:09 -0600

    Code successful determines winner or draw

--- Program.cs
+++ Program.cs
@@ -14,18 +14,34 @@ namespace Jax // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             int gameStatus = 0;
+            int currentPlayer = 1;
             //Announcing the list to hold values for the base game
             List<char> board = new List<char> {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
             while (gameStatus == 0)
             {
-                DisplayBoard(board);
+                if (gameStatus == 0){
+                    DisplayBoard(board);
+                }
 
-                playerOne(board);
+                if (gameStatus == 0){
+                    playerOne(board);
+                    gameStatus = checkForWinner(board,currentPlayer);}
+                currentPlayer = 2;
 
-                DisplayBoard(board);
+                if (gameStatus == 0){
+                    DisplayBoard(board);
+                }
 
-                playerTwo(board);
+                if (gameStatus == 0){
+                    playerTwo(board);
+                    gameStatus = checkForWinner(board,currentPlayer);
+                }
+                currentPlayer = 1;
+
+                if (gameStatus != 0){
+                    DisplayBoard(board);
+                }
 
             }
 
@@ -76,6 +92,49 @@ namespace Jax // Note: actual namespace depends on the project name.
                     }
 
             }
+        }
+
+        //Checks for every winnable solution and if there is a draw.
+        static int checkForWinner(List<char> displayboard, int currentPlayer){
+            int returnValue = 0;
+            if (displayboard[0] == displayboard[1] && displayboard[0] == displayboard[2]){
+                returnValue = 1;
+                Console.WriteLine($"{displayboard[0]}'s win!");
+            }
+            else if (displayboard[3] == displayboard[4] && displayboard[3] == displayboard[5]){
+                returnValue = 1;
+                Console.WriteLine($"{displayboard[3]}'s win!");
+            }
+            else if (displayboard[6] == displayboard[7] && displayboard[6] == displayboard[8]){
+                returnValue = 1;
+                Console.WriteLine($"{displayboard[6]}'s win!");
+            }
+            else if (displayboard[0] == displayboard[3] && displayboard[0] == displayboard[6]){
+                returnValue = 1;
+                Console.WriteLine($"{displayboard[0]}'s win!");
+            }
+            else if (displayboard[1] == displayboard[4] && displayboard[1] == displayboard[7]){
+                returnValue = 1;
+                Console.WriteLine($"{displayboard[1]}'s win!");
+            }
+            else if (displayboard[2] == displayboard[5] && displayboard[2] == displayboard[8]){
+                returnValue = 1;
+                Console.WriteLine($"{displayboard[2]}'s win!");
+            }
+            else if (displayboard[0] == displayboard[4] && displayboard[0] == displayboard[8]){
+                returnValue = 1;
+                Console.WriteLine($"{displayboard[0]}'s win!");
+            }
+            else if (displayboard[2] == displayboard[4] && displayboard[2] == displayboard[6]){
+                returnValue = 1;
+                Console.WriteLine($"{displayboard[2]}'s win!");
+            }
+            else if ((displayboard[0] == 'x' || displayboard[0] == 'o') && (displayboard[1] == 'x' || displayboard[1] == 'o') && (displayboard[2] == 'x' || displayboard[2] == 'o') && (displayboard[3] == 'x' || displayboard[3] == 'o') && (displayboard[4] == 'x' || displayboard[4] == 'o') && (displayboard[5] == 'x' || displayboard[5] == 'o') && (displayboard[6] == 'x' || displayboard[6] == 'o') && (displayboard[7] == 'x' || displayboard[7] == 'o') && (displayboard[8] == 'x' || displayboard[8] == 'o')){
+                returnValue = 1;
+                Console.WriteLine("Draw!");
+            }
+            else{}
+            return returnValue;
         }
     }
 }
